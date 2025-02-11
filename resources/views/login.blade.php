cdeb5b71da2a616c6cf382eed625fd661127bbe8
MRasyaRizki <rascoulgt@gmail.com>
2025-02-11 21:06:48 +0700

    Migration dan model login dan register

---
+++ resources/views/login.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.auth')
+
+@section('main')
+    <div class="d-flex justify-content-center align-items-center vh-100">
+        <form action="" method="POST" class="w-50">
+            <h1 class="text-center my-2">Login</h1>
+            @csrf
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                    name="email">
+            </div>
+
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" name="password">
+            </div>
+            <div class="mb-3">
+                <p class="form-text">
+                    <a href="">Doesn't have an account?</a>
+                </p>
+            </div>
+
+            <button type="submit" class="btn btn-primary w-100">Submit</button>
+        </form>
+    </div>
+@endsection
---
+++ resources/views/register.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.auth')
+
+@section('main')
+    <div class="d-flex justify-content-center align-items-center vh-100">
+        <form action="" method="POST" class="w-50">
+            @csrf
+            <h1 class="text-center my-2">Register</h1>
+            <div class="mb-3">
+                <label for="exampleInputUsername" class="form-label">Username</label>
+                <input type="text" class="form-control" id="exampleInputUsername" name="username">
+            </div>
+
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email">
+                <div id="emailHelp" class="form-text">
+                    We'll never share your email with anyone else.
+                </div>
+            </div>
+
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" name="password">
+            </div>
+            <div class="mb-3">
+                <p class="form-text">
+                    <a href="{{ route('auth.signin') }}">Already have an account?</a>
+                </p>
+            </div>
+
+            <button type="submit" class="btn btn-primary w-100">Submit</button>
+        </form>
+    </div>
+@endsection
